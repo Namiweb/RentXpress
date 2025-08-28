95d89a128443b3148ac79987864aeff43d4e34af
Hasith Kenula <keyhasith800@gmail.com>
2025-08-28 13:02:05 +0530

    initial commit

---
+++ backend/config/dbconnection.js
@@ -0,0 +1,10 @@
+//db connection
+import mongoose from "mongoose";
+
+const dbconnection = async()=>{
+   await mongoose.connect("mongodb+srv://keyhasith800:ZFe1o5l7X4eVn4AP@cluster0.ao4qp8f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+   console.log("DATABASE CONNECTION SUCCESSFULLY!")
+}
+
+
+export default dbconnection;
